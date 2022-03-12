7a4340fbc90ce136db64d6b7f932871a2e65eea6
keithnaglenuigalway <86240520+keithnaglenuigalway@users.noreply.github.com>
2022-03-12 18:51:54 +0000

    Create Shapes.java

---
+++ Shapes.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane; // Dialog box methods
+
+public class Shapes
+{
+	public static void main(String[] args) 
+	{
+    // put your answer for Q2 here
+    System.exit(0); // Terminate program
+	}
+}
