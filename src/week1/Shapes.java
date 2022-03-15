1e4fc8fd2d70257b18104b0d2e03df8958dbb37a
keithnaglenuigalway <86240520+keithnaglenuigalway@users.noreply.github.com>
2022-03-15 20:13:22 +0000

    Create Shapes.java

---
+++ src/week1/Shapes.java
@@ -0,0 +1,12 @@
+package week1;
+
+import javax.swing.JOptionPane; // Dialog box methods
+
+public class Shapes {
+    public static void main(String[] args) {
+        // replace the contents of this file with your code for Q2
+        // Do NOT upload new files and ignore this one
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+        System.exit(0); // Terminate program
+    }
+}
